565552ed86e637690aa886a9f9e5a1500b07d06e
Sam Jarrett <samstah@gmail.com>
2011-11-08 20:29:38 +1100

    Check in complete presentation!

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Author: Sam Jarrett (samstah@gmail.com) */
+
+$(function() {
+	$('#res').html('<p>Width: '+$(window).width()+'<br>Height: '+$(window).height()+'</p>');
+	$('#presentation').fathom();
+	prettyPrint();
+});
